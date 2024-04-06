95163770a7274222106bd10a1fc013c6c12ce8e1
Taleh <taleh.ahmed2004@gmail.com>
2024-04-06 16:57:39 +0400

    first touch

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const submitButton = document.querySelector(`.body .addButton`);
+const ul = document.querySelector(`ul`);
+const li = document.querySelector(`li`)
+console.log(submitButton);
+console.log(ul);
+console.log(li);
+
+
+submitButton.addEventListener(`click`,(event)=>{
+    let newLi = document.createAttribute(`li`);
+})

029b6fa5de2c1e9305f89626d1fa6d58be5d3a60
Taleh <taleh.ahmed2004@gmail.com>
2024-04-08 09:36:52 +0400

    Elave etmek ve silmek xususiyyetleri nisbeten hazirlandi

--- js/main.js
+++ js/main.js
@@ -1,11 +1,59 @@
-const submitButton = document.querySelector(`.body .addButton`);
+//birinci x i duzeltmek isteyirem
+const removeList = document.querySelectorAll(`ul button`);
+const list = document.querySelectorAll(`li`);
+console.log(removeList);
+console.log(list);
+
+//helelik bu qeder. !Problem x e basilanda butun listler silinir. Arasdirr
+removeList.forEach((button) => {
+  button.addEventListener(`click`, (element) => {
+    console.log(button);
+    list.forEach((li) => {
+      li.remove();
+    });
+  });
+});
+
+// input elave etmek hissesi :
+
+const inputButton = document.querySelector(`.button .input`);
 const ul = document.querySelector(`ul`);
-const li = document.querySelector(`li`)
-console.log(submitButton);
-console.log(ul);
-console.log(li);
+// console.log(inputButton);
+// console.log(ul);
 
+inputButton.addEventListener(`click`, () => {
+  let newLi = document.createElement(`li`);
+  let newButton = document.createElement(`button`);
+  let newInput = document.createElement(`input`);
+  newButton.type = `button`;
+  newButton.innerText = `x`;
+  newInput.placeholder = `Notunuzu daxil edin...`;
+  newInput.type = `text`;
+  newInput.name = `input-text`;
+  newInput.classList.add(`input-text`);
 
-submitButton.addEventListener(`click`,(event)=>{
-    let newLi = document.createAttribute(`li`);
-})
+  newLi.append(newButton, newInput);
+  ul.append(newLi);
+  console.log(ul);
+});
+
+//Elave et duymesinin strukturunu qurmaga calisacam
+
+//Elave et dediymiz zaman diger inputlar ardiq elcatmaz olmalidir... y
+//ya da olmali deyil ??
+//elave et duymesinin meqsedi inputun daxilinde yazilan metnin input daxilinde qalmagidi
+//hee onda inputumuzda artiq deyisikliyin qarsisi alinmalidi duymeye kliklendiyi zaman
+//formdan istifade etmeliyik.
+const inputAddButton = document.querySelector(`.button .addition`);
+// const inputForm = document.querySelector(`.input-form`)
+// console.log(inputAddButton)
+inputAddButton.addEventListener(`click`, (event) => {
+
+  const data = new FormData(event.target);
+  console.log(data);
+});
+// inputForm.addEventListener((`submit`,(event)=>{
+//     // const data = new FormData(event.target);
+//     // console.log(data.get(`input-text`));
+// }))
+//PROBLEM: elave edilen lileri silmek olmur!!!
